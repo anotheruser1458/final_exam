11a8056c861d3e570ed45072c9494d06f1ad60b2
anotheruser1458 <maxwellpauly32@gmail.com>
2022-05-12 21:54:07 -1000

    hello world running

---
+++ test.cpp
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  final_exam - EE 205 - Spr 2022
+///
+/// @file test.cpp
+/// @version 1.0
+///
+/// @author Maxwell Pauly <mgpauly@hawaii.edu>
+/// @date   12_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    return 0;
+}
